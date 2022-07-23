3879fac92bf8644d536454877d495c2fcd32748e
Gangadhar <parimi.gangadhar8@gmail.com>
2022-07-23 16:21:08 +0530

    Commit StreamPractice Priority Queue

--- src/main/java/com/example/functionalinterface/PriorityQueueImpl.java
+++ src/main/java/com/example/functionalinterface/PriorityQueueImpl.java
@@ -24,20 +24,21 @@ public class PriorityQueueImpl {
                 (new EmployeeBean(3,"sai","allam",4000,"Accounts")),
                 (new EmployeeBean(4,"bha","kami",5000,"IT")),
                 (new EmployeeBean(5,"babji","kolli",6000,"Account")),
-                (new EmployeeBean(6,"bhi","kolla",2000,"IT")),
-                (new EmployeeBean(7,"kiran","barre",8000,"Executive")),
-                (new EmployeeBean(8,"jaya","nalla",9000,"IT")),
-                (new EmployeeBean(9,"satti","chetta",1500,"Executive"))));
-        employeeBeans.add(new EmployeeBean(10,"hhh","cccc",1800,"Executive"));
+                (new EmployeeBean(10,"hhh","cccc",1800,"Executive")),
+                (new EmployeeBean(6,"bhi","kolla",2000,"IT"))
+                ));
+
         employeeBeans.forEach(System.out::println);
 
         PriorityQueue<Integer> integers=new PriorityQueue<>();
-        integers.add(6);
-        integers.add(1);
-        integers.add(2);
-        integers.add(4);
-        integers.add(9);
-        integers.add(5);
+        integers.add(54);
+        integers.add(17);
+        integers.add(82);
+        integers.add(64);
+        integers.add(91);
+        integers.add(10);
+        integers.add(36);
+        integers.add(45);
         integers.forEach(System.out::println);
 
 
